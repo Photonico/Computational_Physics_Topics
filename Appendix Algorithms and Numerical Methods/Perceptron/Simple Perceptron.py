9fe7efe4683c4ebbf0ab6c55ffc4955fa717a585
Photonico <LukeNiu@outlook.com>
2022-05-19 14:19:04 +1000

    Update

--- Appendix Algorithms and Numerical Methods/Perceptron/Simple Perceptron.py
+++ Appendix Algorithms and Numerical Methods/Perceptron/Simple Perceptron.py
@@ -3,20 +3,6 @@
 import numpy as np
 
 # Received an AND function with parameters (x1, x2)
-def AND(x1,x2):
-    w1 = 0.5
-    w2 = 0.5
-    θ  = 0.75
-    t  = x1*w1 + x2*w2
-    if t <= θ:
-        return 0
-    elif t > θ:
-        return 1
-
-print(AND(0,0))     # 0
-print(AND(0,1))     # 0
-print(AND(1,0))     # 0
-print(AND(1,1))     # 1
 
 def OR(x1,x2):
     w1 = 0.5

fa3e7f432f330c8ded0416c34b4c1fd18449bb4f
Photonico <LukeNiu@outlook.com>
2023-02-28 18:34:29 +1100

    Update

---
+++ Appendix Data Manipulation/2D Plotting in Python.py
@@ -0,0 +1,7 @@
+# 2D  Plotting in Python: Citys Climate
+
+import numpy as np
+
+Beijing_temp_max = np.array([0,0,0,0,0,0,0,0,0,0,0,0])
+Beijing_temp_min = np.array([0,0,0,0,0,0,0,0,0,0,0,0])
+Beijing_precipitation = np.array([0,0,0,0,0,0,0,0,0,0,0,0])

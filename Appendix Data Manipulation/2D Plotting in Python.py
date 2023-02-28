951298a2b224aeaeb19d3c91bc2d072a4f6edacc
Photonico <LukeNiu@outlook.com>
2023-02-28 18:43:15 +1100

    Update

--- Appendix Data Manipulation/2D Plotting in Python.py
+++ Appendix Data Manipulation/2D Plotting in Python.py
@@ -2,7 +2,10 @@
 
 import numpy as np
 
-Beijing_temp_max = np.array([1,4,0,0,0,0,0,0,0,0,0,0])
-Beijing_temp_ave = np.array([1,4,0,0,0,0,0,0,0,0,0,0])
-Beijing_temp_min = np.array([-10,0,0,0,0,0,0,0,0,0,0,0])
-Beijing_precipitation = np.array([0,0,0,0,0,0,0,0,0,0,0,0])
+# Beijing
+Beijing_temp_max        = np.array([  1,  4, 11, 20, 27, 31, 32, 31, 27, 20, 10,  3])
+Beijing_temp_min        = np.array([-10, -7, -2,  6, 13, 18, 22, 20, 14,  7, -2, -8])
+Beijing_temp_ave        = np.array([ -5, -2,  4, 13, 20, 24, 26, 24, 20, 13,  4, -2])
+Beijing_precipitation   = np.array([  3,  4,  8, 22, 35, 78,170,152, 43, 12,  3,  2])
+
+# Sydney

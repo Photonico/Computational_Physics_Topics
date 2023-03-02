4ede05823b2a2a5c6e6c8e70bd6aecf01d6ad46f
Photonico <LukeNiu@outlook.com>
2023-03-02 23:37:12 +1100

    Update

--- Appendix Data Manipulation/2D  Plotting in Python: Citys Climate 2023.py
+++ Appendix Data Manipulation/2D  Plotting in Python: Citys Climate 2023.py
@@ -4,9 +4,9 @@ import numpy as np
 
 # Beijing
 Beijing_temp_max        = np.array([ -1.1,  2.3,  9.1, 18.4, 25.0, 30.1, 31.9, 30.8, 26.2, 18.5,  8.3,  0.5])
-Beijing_temp_min        = np.array([-10, -7, -2,  6, 13, 18, 22, 20, 14,  7, -2, -8])
-Beijing_temp_ave        = np.array([ -5, -2,  4, 13, 20, 24, 26, 24, 20, 13,  4, -2])
-Beijing_precipitation   = np.array([  3,  4,  8, 22, 35, 78,170,152, 43, 12,  3,  2])
+Beijing_temp_min        = np.array([-10.0, -7.7, -1.6,  6.4, 13.8, 19.3, 23.1, 22.3, 15.5,  6.9, -2.2, -8.2])
+Beijing_temp_ave        = np.array([ -5.5, -2.8,  3.7, 12.4, 19.4, 24.7, 27.4, 26.5, 20.6, 12.7,  3.1, -3.9])
+Beijing_precipitation   = np.array([  3.0,  4.0,  8.0, 25.0, 44.0, 78.0,185.0,165.0, 37.0, 12.0,  3.0,  2.0])
 
 # Shanghai
 Shanghai_temp_max       = np.array([  8.3,  9.2, 12.8, 18.1, 23.0, 27.1, 31.1, 31.3, 27.4, 22.8, 17.6, 12.5])
@@ -15,8 +15,8 @@ Shanghai_temp_ave       = np.array([  4.3,  5.3,  9.0, 14.4, 19.4, 23.6, 27.6, 2
 Shanghai_precipitation  = np.array([ 53.0, 59.0, 94.0, 87.0, 89.0,152.0,221.0,157.0,121.0, 63.0, 46.0, 43.0])
 
 # Sydney
-Sydney_temp_max         = np.array([ 26, 26, 25, 23, 20, 17, 17, 18, 21, 23, 24, 25])
-Sydney_temp_min         = np.array([ 19, 19, 18, 15, 12,  9,  8,  9, 11, 13, 16, 18])
+Sydney_temp_max         = np.array([ 26.6, 26.5, 25.1, 22.6, 19.2, 16.4, 15.6, 17.1, 19.2, 21.5, 23.2, 25.2])
+Sydney_temp_min         = np.array([ 19.5, 19.7, 18.8, 15.6, 12.2,  9.4,  8.5,  9.4, 11.3, 13.6, 16.2, 18.5])
 Sydney_temp_ave         = np.array([ 22, 23, 21, 19, 17, 15, 14, 15, 17, 19, 21, 22])
 Sydney_precipitation    = np.array([104,111,129,126,120,132, 85, 89, 68, 83, 80, 73])
 

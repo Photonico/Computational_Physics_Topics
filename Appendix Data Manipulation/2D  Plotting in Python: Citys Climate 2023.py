c02d400a4479b769214c8849095af52a15e7e059
Photonico <LukeNiu@outlook.com>
2023-03-02 23:15:32 +1100

    Update

--- Appendix Data Manipulation/2D  Plotting in Python: Citys Climate 2023.py
+++ Appendix Data Manipulation/2D  Plotting in Python: Citys Climate 2023.py
@@ -9,10 +9,10 @@ Beijing_temp_ave        = np.array([ -5, -2,  4, 13, 20, 24, 26, 24, 20, 13,  4,
 Beijing_precipitation   = np.array([  3,  4,  8, 22, 35, 78,170,152, 43, 12,  3,  2])
 
 # Sydney
-Sydney_temp_max        = np.array([  1,  4, 11, 20, 27, 31, 32, 31, 27, 20, 10,  3])
-Sydney_temp_min        = np.array([-10, -7, -2,  6, 13, 18, 22, 20, 14,  7, -2, -8])
-Sydney_temp_ave        = np.array([ -5, -2,  4, 13, 20, 24, 26, 24, 20, 13,  4, -2])
-Sydney_precipitation   = np.array([  3,  4,  8, 22, 35, 78,170,152, 43, 12,  3,  2])
+Sydney_temp_max         = np.array([ 26, 26, 25, 23, 20, 17, 17, 18, 21, 23, 24, 25])
+Sydney_temp_min         = np.array([ 19, 19, 18, 15, 12,  9,  8,  9, 11, 13, 16, 18])
+Sydney_temp_ave         = np.array([ 22, 23, 21, 19, 17, 15, 14, 15, 17, 19, 21, 22])
+Sydney_precipitation    = np.array([104,111,129,126,120,132, 85, 89, 68, 83, 80, 73])
 
 # Shanghai
 
